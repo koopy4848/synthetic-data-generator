ff88291f2caec156724a9e73ae1b82c305e05823
Radu Stefanescu <koopy4848@gmail.com>
2023-11-15 08:11:19 +0200

    basic site structure and design

--- webapp/app.py
+++ webapp/app.py
@@ -1,11 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    return render_template('home.html')
+
+
+@app.route('/documentation')
+def documentation():
+    return render_template('home.html')
+
+
+@app.route('/generator_one')
+def generator_one():
+    return render_template('generate.html')
+
+
+@app.route('/generator_two')
+def generator_two():
+    return render_template('generate.html')
+
+
+@app.route('/generator_three')
+def generator_three():
+    return render_template('generate.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
