6b8a71fc39a023043428c45cf970ab92d2109535
Radu Stefanescu <koopy4848@gmail.com>
2023-11-22 21:17:25 +0200

    more stupid fixes 2

--- src/dataflow_sdg_bq/main.py
+++ src/dataflow_sdg_bq/main.py
@@ -20,35 +20,24 @@ class CustomJSONEncoder(json.JSONEncoder):
         return json.JSONEncoder.default(self, obj)
 
 
-
-
-class Field:
-    def __init__(self, field_id, faker_method, display, data_type, example):
-        self.field_id = field_id
-        self.faker_method = faker_method
-        self.display = display
-        self.data_type = data_type
-        self.example = example
-
-
 class ProcessMessage(beam.DoFn):
     def __init__(self):
         self.field_definitions = {
-            "first_name": Field('first_name', 'first_name', 'First Name', str, 'Vasile'),
-            "last_name": Field('last_name', 'last_name', 'Last Name', str, 'Popescu'),
-            "personal_number": Field('personal_number', 'ssn', 'Social Personal Number', int, '8240804276204'),
-            "birthdate": Field('birthdate', 'date_of_birth', 'Date', 'date', '2023-04-19'),
-            "address": Field('address', 'address', 'Address with Postcode', str, 'Intrarea Nr. 167 Nicolau Mare, 197205'),
-            "county": Field('county', 'state', 'County Name', str, 'Hunedoara'),
-            "phone_number": Field('phone_number', 'phone_number', 'Phone Number', str, '0248 455 730'),
-            "mac_address": Field('mac_address', 'mac_address', 'MAC Address', str, 'd4:a3:82:98:85:a6'),
-            "ip_address": Field('ip_address', 'ipv4', 'IP Address', str, '172.25.185.30'),
-            "job": Field('job', 'job', 'Job Title', str, 'Pictor'),
-            "iban": Field('iban', 'iban', 'Bank Account Number', str, 'RO83YUNU3862659030037214'),
-            "currency": Field('currency', 'currency_code', 'Currency Code', str, 'XAF'),
-            "balance": Field('balance', 'random_number', 'Random Number', int, '75564642'),
-            "latitude": Field('latitude', 'latitude', 'Latitude Value', float, '27.6863225'),
-            "longitude": Field('longitude', 'longitude', 'Longitude Value', float, '44.431989')
+            "first_name": ('first_name', 'first_name', 'First Name', str, 'Vasile'),
+            "last_name": ('last_name', 'last_name', 'Last Name', str, 'Popescu'),
+            "personal_number": ('personal_number', 'ssn', 'Social Personal Number', int, '8240804276204'),
+            "birthdate": ('birthdate', 'date_of_birth', 'Date', 'date', '2023-04-19'),
+            "address": ('address', 'address', 'Address with Postcode', str, 'Intrarea Nr. 167 Nicolau Mare, 197205'),
+            "county": ('county', 'state', 'County Name', str, 'Hunedoara'),
+            "phone_number": ('phone_number', 'phone_number', 'Phone Number', str, '0248 455 730'),
+            "mac_address": ('mac_address', 'mac_address', 'MAC Address', str, 'd4:a3:82:98:85:a6'),
+            "ip_address": ('ip_address', 'ipv4', 'IP Address', str, '172.25.185.30'),
+            "job": ('job', 'job', 'Job Title', str, 'Pictor'),
+            "iban": ('iban', 'iban', 'Bank Account Number', str, 'RO83YUNU3862659030037214'),
+            "currency": ('currency', 'currency_code', 'Currency Code', str, 'XAF'),
+            "balance": ('balance', 'random_number', 'Random Number', int, '75564642'),
+            "latitude": ('latitude', 'latitude', 'Latitude Value', float, '27.6863225'),
+            "longitude": ('longitude', 'longitude', 'Longitude Value', float, '44.431989')
         }
 
     def invoke_method(self, obj, method_name):
@@ -90,11 +79,11 @@ class ProcessMessage(beam.DoFn):
             field_id = field[0]
             field_info = self.field_definitions[field_id]
             field_schema = {
-                'name': field[1] if field[1] else field_info.display,
-                'type': 'STRING' if field_info.data_type in [str, 'str'] else
-                'INTEGER' if field_info.data_type in [int, 'int'] else
-                'FLOAT' if field_info.data_type in [float, 'float'] else
-                'DATE' if field_info.data_type in ['date'] else
+                'name': field[1] if field[1] else field_info[2],
+                'type': 'STRING' if field_info[3] in [str, 'str'] else
+                'INTEGER' if field_info[3] in [int, 'int'] else
+                'FLOAT' if field_info[3] in [float, 'float'] else
+                'DATE' if field_info[3] in ['date'] else
                 'STRING',  # Default type
                 'mode': 'NULLABLE'  # Assuming all fields are nullable
             }
@@ -107,9 +96,9 @@ class ProcessMessage(beam.DoFn):
         field_data = message['field_data']
         rows = message['rows']
 
-        faker_methods = [self.field_definitions[field[0]].faker_method for field in field_data]
-        headers = [field[1] if field[1] else self.field_definitions[field[0]].display for field in field_data]
-        schema = self.generate_bigquery_schema_from_message(field_data, self.field_definitions)
+        faker_methods = [self.field_definitions[field[0]][1] for field in field_data]
+        headers = [field[1] if field[1] else self.field_definitions[field[0]][2] for field in field_data]
+        schema = self.generate_bigquery_schema_from_message(field_data)
         for _ in range(rows):
             row_data = self.row_to_dict(faker_methods, headers)
             key = (bq_table, json.dumps(schema))  # Use JSON string as the schema can be a complex object
