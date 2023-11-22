1b8a5a1fbf9fdff1d6e08c7ceb8357f2345a94ca
Radu Stefanescu <koopy4848@gmail.com>
2023-11-22 17:20:09 +0200

    trying out simpler versions of classes so they can be serialized

--- src/dataflow_sdg_bq/main.py
+++ src/dataflow_sdg_bq/main.py
@@ -32,8 +32,24 @@ class Field:
 
 
 class ProcessMessage(beam.DoFn):
-    def __init__(self, field_defs):
-        self.field_definitions = field_defs
+    def __init__(self):
+        self.field_definitions = {
+            "first_name": Field('first_name', 'first_name', 'First Name', str, 'Vasile'),
+            "last_name": Field('last_name', 'last_name', 'Last Name', str, 'Popescu'),
+            "personal_number": Field('personal_number', 'ssn', 'Social Personal Number', int, '8240804276204'),
+            "birthdate": Field('birthdate', 'date_of_birth', 'Date', 'date', '2023-04-19'),
+            "address": Field('address', 'address', 'Address with Postcode', str, 'Intrarea Nr. 167 Nicolau Mare, 197205'),
+            "county": Field('county', 'state', 'County Name', str, 'Hunedoara'),
+            "phone_number": Field('phone_number', 'phone_number', 'Phone Number', str, '0248 455 730'),
+            "mac_address": Field('mac_address', 'mac_address', 'MAC Address', str, 'd4:a3:82:98:85:a6'),
+            "ip_address": Field('ip_address', 'ipv4', 'IP Address', str, '172.25.185.30'),
+            "job": Field('job', 'job', 'Job Title', str, 'Pictor'),
+            "iban": Field('iban', 'iban', 'Bank Account Number', str, 'RO83YUNU3862659030037214'),
+            "currency": Field('currency', 'currency_code', 'Currency Code', str, 'XAF'),
+            "balance": Field('balance', 'random_number', 'Random Number', int, '75564642'),
+            "latitude": Field('latitude', 'latitude', 'Latitude Value', float, '27.6863225'),
+            "longitude": Field('longitude', 'longitude', 'Longitude Value', float, '44.431989')
+        }
 
     def invoke_method(self, obj, method_name):
         # Check if the method exists in the object
@@ -132,28 +148,10 @@ def run(argv=None):
     pipeline_options = PipelineOptions(argv)
     subscription = os.getenv("BQ_SUB")
 
-    field_definitions = {
-        "first_name": Field('first_name', 'first_name', 'First Name', str, 'Vasile'),
-        "last_name": Field('last_name', 'last_name', 'Last Name', str, 'Popescu'),
-        "personal_number": Field('personal_number', 'ssn', 'Social Personal Number', int, '8240804276204'),
-        "birthdate": Field('birthdate', 'date_of_birth', 'Date', 'date', '2023-04-19'),
-        "address": Field('address', 'address', 'Address with Postcode', str, 'Intrarea Nr. 167 Nicolau Mare, 197205'),
-        "county": Field('county', 'state', 'County Name', str, 'Hunedoara'),
-        "phone_number": Field('phone_number', 'phone_number', 'Phone Number', str, '0248 455 730'),
-        "mac_address": Field('mac_address', 'mac_address', 'MAC Address', str, 'd4:a3:82:98:85:a6'),
-        "ip_address": Field('ip_address', 'ipv4', 'IP Address', str, '172.25.185.30'),
-        "job": Field('job', 'job', 'Job Title', str, 'Pictor'),
-        "iban": Field('iban', 'iban', 'Bank Account Number', str, 'RO83YUNU3862659030037214'),
-        "currency": Field('currency', 'currency_code', 'Currency Code', str, 'XAF'),
-        "balance": Field('balance', 'random_number', 'Random Number', int, '75564642'),
-        "latitude": Field('latitude', 'latitude', 'Latitude Value', float, '27.6863225'),
-        "longitude": Field('longitude', 'longitude', 'Longitude Value', float, '44.431989')
-    }
-
     with beam.Pipeline(options=pipeline_options) as p:
         (p
          | "Read from Pub/Sub" >> beam.io.ReadFromPubSub(subscription=subscription)
-         | "Process Messages" >> beam.ParDo(ProcessMessage(field_defs=field_definitions))
+         | "Process Messages" >> beam.ParDo(ProcessMessage())
          | "Apply Windowing" >> beam.WindowInto(window.FixedWindows(10))  # 10 seconds windows
          | "Group by Table and Schema" >> beam.GroupByKey()
          | "Write Grouped Data to BigQuery" >> beam.ParDo(WriteGroupedDataToBigQueryFn())
