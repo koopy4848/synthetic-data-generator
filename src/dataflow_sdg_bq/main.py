0b9d80e8ab1a72077dc226a40fd2f1bb046e35b0
Radu Stefanescu <koopy4848@gmail.com>
2023-11-22 16:36:29 +0200

    moved field_definitions to be passed as param instead of global var

--- src/dataflow_sdg_bq/main.py
+++ src/dataflow_sdg_bq/main.py
@@ -58,35 +58,19 @@ class Field:
         self.example = example
 
 
-field_definitions = {
-    "first_name": Field('first_name', 'first_name', 'First Name', str, 'Vasile'),
-    "last_name": Field('last_name', 'last_name', 'Last Name', str, 'Popescu'),
-    "personal_number": Field('personal_number', 'ssn', 'Social Personal Number', int, '8240804276204'),
-    "birthdate": Field('birthdate', 'date_of_birth', 'Date', 'date', '2023-04-19'),
-    "address": Field('address', 'address', 'Address with Postcode', str, 'Intrarea Nr. 167 Nicolau Mare, 197205'),
-    "county": Field('county', 'state', 'County Name', str, 'Hunedoara'),
-    "phone_number": Field('phone_number', 'phone_number', 'Phone Number', str, '0248 455 730'),
-    "mac_address": Field('mac_address', 'mac_address', 'MAC Address', str, 'd4:a3:82:98:85:a6'),
-    "ip_address": Field('ip_address', 'ipv4', 'IP Address', str, '172.25.185.30'),
-    "job": Field('job', 'job', 'Job Title', str, 'Pictor'),
-    "iban": Field('iban', 'iban', 'Bank Account Number', str, 'RO83YUNU3862659030037214'),
-    "currency": Field('currency', 'currency_code', 'Currency Code', str, 'XAF'),
-    "balance": Field('balance', 'random_number', 'Random Number', int, '75564642'),
-    "latitude": Field('latitude', 'latitude', 'Latitude Value', float, '27.6863225'),
-    "longitude": Field('longitude', 'longitude', 'Longitude Value', float, '44.431989')
-}
-
-
 class ProcessMessage(beam.DoFn):
+    def __init__(self, field_defs):
+        self.field_definitions = field_defs
+
     def process(self, element):
         message = json.loads(element)
         bq_table = message['bq_table']
         field_data = message['field_data']
         rows = message['rows']
 
-        faker_methods = [field_definitions[field[0]].faker_method for field in field_data]
-        headers = [field[1] if field[1] else field_definitions[field[0]].display for field in field_data]
-        schema = generate_bigquery_schema_from_message(field_data, field_definitions)
+        faker_methods = [self.field_definitions[field[0]].faker_method for field in field_data]
+        headers = [field[1] if field[1] else self.field_definitions[field[0]].display for field in field_data]
+        schema = generate_bigquery_schema_from_message(field_data, self.field_definitions)
         for _ in range(rows):
             row_data = row_to_dict(faker_methods, headers)
             key = (bq_table, json.dumps(schema))  # Use JSON string as the schema can be a complex object
@@ -149,6 +133,24 @@ class WriteGroupedDataToBigQueryFn(beam.DoFn):
 def run(argv=None):
     pipeline_options = PipelineOptions(argv)
     subscription = os.getenv("BQ_SUB")
+
+    field_definitions = {
+        "first_name": Field('first_name', 'first_name', 'First Name', str, 'Vasile'),
+        "last_name": Field('last_name', 'last_name', 'Last Name', str, 'Popescu'),
+        "personal_number": Field('personal_number', 'ssn', 'Social Personal Number', int, '8240804276204'),
+        "birthdate": Field('birthdate', 'date_of_birth', 'Date', 'date', '2023-04-19'),
+        "address": Field('address', 'address', 'Address with Postcode', str, 'Intrarea Nr. 167 Nicolau Mare, 197205'),
+        "county": Field('county', 'state', 'County Name', str, 'Hunedoara'),
+        "phone_number": Field('phone_number', 'phone_number', 'Phone Number', str, '0248 455 730'),
+        "mac_address": Field('mac_address', 'mac_address', 'MAC Address', str, 'd4:a3:82:98:85:a6'),
+        "ip_address": Field('ip_address', 'ipv4', 'IP Address', str, '172.25.185.30'),
+        "job": Field('job', 'job', 'Job Title', str, 'Pictor'),
+        "iban": Field('iban', 'iban', 'Bank Account Number', str, 'RO83YUNU3862659030037214'),
+        "currency": Field('currency', 'currency_code', 'Currency Code', str, 'XAF'),
+        "balance": Field('balance', 'random_number', 'Random Number', int, '75564642'),
+        "latitude": Field('latitude', 'latitude', 'Latitude Value', float, '27.6863225'),
+        "longitude": Field('longitude', 'longitude', 'Longitude Value', float, '44.431989')
+    }
 
     with beam.Pipeline(options=pipeline_options) as p:
         (p
--- src/dataflow_sdg_gcs/main.py
+++ src/dataflow_sdg_gcs/main.py
@@ -1,5 +1,5 @@
 import apache_beam as beam
-from apache_beam.options.pipeline_options import PipelineOptions, SetupOptions
+from apache_beam.options.pipeline_options import PipelineOptions
 import json
 from datetime import date
 from decimal import Decimal
@@ -57,26 +57,13 @@ class Field:
         self.example = example
 
 
-field_definitions = {
-    "first_name": Field('first_name', 'first_name', 'First Name', str, 'Vasile'),
-    "last_name": Field('last_name', 'last_name', 'Last Name', str, 'Popescu'),
-    "personal_number": Field('personal_number', 'ssn', 'Social Personal Number', int, '8240804276204'),
-    "birthdate": Field('birthdate', 'date_of_birth', 'Date', 'date', '2023-04-19'),
-    "address": Field('address', 'address', 'Address with Postcode', str, 'Intrarea Nr. 167 Nicolau Mare, 197205'),
-    "county": Field('county', 'state', 'County Name', str, 'Hunedoara'),
-    "phone_number": Field('phone_number', 'phone_number', 'Phone Number', str, '0248 455 730'),
-    "mac_address": Field('mac_address', 'mac_address', 'MAC Address', str, 'd4:a3:82:98:85:a6'),
-    "ip_address": Field('ip_address', 'ipv4', 'IP Address', str, '172.25.185.30'),
-    "job": Field('job', 'job', 'Job Title', str, 'Pictor'),
-    "iban": Field('iban', 'iban', 'Bank Account Number', str, 'RO83YUNU3862659030037214'),
-    "currency": Field('currency', 'currency_code', 'Currency Code', str, 'XAF'),
-    "balance": Field('balance', 'random_number', 'Random Number', int, '75564642'),
-    "latitude": Field('latitude', 'latitude', 'Latitude Value', float, '27.6863225'),
-    "longitude": Field('longitude', 'longitude', 'Longitude Value', float, '44.431989')
-}
-
-
 class WriteDataToCloudStorageFn(beam.DoFn):
+    def __init__(self, field_definitions, *unused_args, **unused_kwargs):
+        super().__init__(unused_args, unused_kwargs)
+        self.field_definitions = field_definitions
+        self.client = None
+        self.gcs_bucket = None
+
     def start_bundle(self):
         self.gcs_bucket = os.getenv('BUCKET_NAME', 'default_bucket')
         self.client = storage.Client()
@@ -88,8 +75,8 @@ class WriteDataToCloudStorageFn(beam.DoFn):
         field_data = message['field_data']
         rows = message['rows']
 
-        faker_methods = [field_definitions[field[0]].faker_method for field in field_data]
-        headers = [field[1] if field[1] else field_definitions[field[0]].display for field in field_data]
+        faker_methods = [self.field_definitions[field[0]].faker_method for field in field_data]
+        headers = [field[1] if field[1] else self.field_definitions[field[0]].display for field in field_data]
 
         bucket = self.client.bucket(self.gcs_bucket)
         blob = bucket.blob(file_name)
@@ -127,10 +114,28 @@ def run(argv=None):
     pipeline_options = PipelineOptions(argv)
     subscription = os.getenv("GCS_SUB")
 
+    field_definitions = {
+        "first_name": Field('first_name', 'first_name', 'First Name', str, 'Vasile'),
+        "last_name": Field('last_name', 'last_name', 'Last Name', str, 'Popescu'),
+        "personal_number": Field('personal_number', 'ssn', 'Social Personal Number', int, '8240804276204'),
+        "birthdate": Field('birthdate', 'date_of_birth', 'Date', 'date', '2023-04-19'),
+        "address": Field('address', 'address', 'Address with Postcode', str, 'Intrarea Nr. 167 Nicolau Mare, 197205'),
+        "county": Field('county', 'state', 'County Name', str, 'Hunedoara'),
+        "phone_number": Field('phone_number', 'phone_number', 'Phone Number', str, '0248 455 730'),
+        "mac_address": Field('mac_address', 'mac_address', 'MAC Address', str, 'd4:a3:82:98:85:a6'),
+        "ip_address": Field('ip_address', 'ipv4', 'IP Address', str, '172.25.185.30'),
+        "job": Field('job', 'job', 'Job Title', str, 'Pictor'),
+        "iban": Field('iban', 'iban', 'Bank Account Number', str, 'RO83YUNU3862659030037214'),
+        "currency": Field('currency', 'currency_code', 'Currency Code', str, 'XAF'),
+        "balance": Field('balance', 'random_number', 'Random Number', int, '75564642'),
+        "latitude": Field('latitude', 'latitude', 'Latitude Value', float, '27.6863225'),
+        "longitude": Field('longitude', 'longitude', 'Longitude Value', float, '44.431989')
+    }
+
     with beam.Pipeline(options=pipeline_options) as p:
         (p
          | "Read from Pub/Sub" >> beam.io.ReadFromPubSub(subscription=subscription)
-         | "Process Messages" >> beam.ParDo(WriteDataToCloudStorageFn())
+         | "Process Messages" >> beam.ParDo(WriteDataToCloudStorageFn(field_definitions))
          )
 
 
